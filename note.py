2ed10afaca184033216cd107a3474538c39ef3d5
Max Sun <maxlouiesun@gmail.com>
2020-02-09 23:42:43 -0800

    updated note to reflect the readme

--- note.py
+++ note.py
@@ -1,4 +1,4 @@
-# from collections import defaultdict
+'''A note parser'''
 from dataclasses import dataclass
 from pprint import pprint
 from typing import List, Callable, Dict, FrozenSet, Tuple, Optional, Union
@@ -8,12 +8,14 @@ import hashlib
 
 @dataclass(frozen=True)
 class Token:
+    '''Represents a value of a certain type'''
     type: str
     value: str
 
 
 @dataclass(frozen=True)
 class Expression:
+    '''Represents a list of Tokens with a set of properties'''
     content: List[Token]
     properties: Dict[str, Union[str, int]]
 
@@ -31,7 +33,7 @@ Context = FrozenSet[Expression]
 
 Resolver = Callable[[Token, Context], Context]
 
-Linker = Callable[[Expression, Context], Context]
+Linker = Callable[[Context, Context], Context]
 
 
 def tokenize(text: str, groups: Dict[str, Dict[str, str]]) -> List[Token]:
@@ -69,7 +71,7 @@ def tokenize(text: str, groups: Dict[str, Dict[str, str]]) -> List[Token]:
 def parse(
         lines: List[str],
         token_patterns: Dict[str, Dict[str, str]]) -> Context:
-    '''Tokenizes + converts a list of plaintexts into a list of blocks'''
+    '''Tokenizes + converts a list of texts into a Context'''
     context = []
     for idx, line in enumerate([x for x in lines if x != '']):
         context.append(Expression(
@@ -103,50 +105,72 @@ context = parse(re.split(r'(\s*\-.*\n)', '''
 - The [[Moon]] orbits the [[World]]
     - Our [[World]] is called [[Earth]]
     - Pretty \\cool
-- [[Earth]] orbits the [[Sun]]'''), TOKEN_REGEX)
+- [[Earth]] orbits the [[Sun]]
+- [[Sun]]
+    - is a star
+'''), TOKEN_REGEX)
 
 
-def sort_by_property(context, prop_name):
+def sort_by_property(context: Context, prop_name: str) -> List[Expression]:
+    '''Returns <context> as a list, sorted by <prop_name>'''
     return list(sorted(context, key=lambda x: x.properties[prop_name]))
 
 
-def generate_id(exp: Expression) -> str:
+def parse_id(exp: Expression) -> str:
     '''Returns a consistent hash for an expression'''
     return str(hash(exp))
 
 
 def resolve_id(_id: str, ctx: Context) -> Context:
+    '''Returns the subset of <ctx> with <_id>'''
     for exp in ctx:
-        if generate_id(exp) == _id:
+        if parse_id(exp) == _id:
             return frozenset([exp])
     return frozenset()
 
 
-def resolve_reference(ref_value: str, ctx: Context) -> Optional[Expression]:
+def resolve_reference(ref_value: str, ctx: Context) -> Context:
+    '''Returns the subset of <ctx> pointed to by <ref_value>'''
+    refs = []
     for exp in ctx:
         if len(exp.content) == 2 and exp.content[0].type == 'indent':
             if exp.content[1].type == 'ref' and exp.content[1].value == ref_value:
-                return exp
+                refs.append(exp)
+    if len(refs) > 0:
+        return frozenset(refs)
+    return frozenset([Expression([Token('ref', ref_value)], {'index': -1})])
 
 
-def get_indent(exp: Expression) -> int:
-    '''Returns the amount of space before a "-" in a textblock'''
+def get_references(subcontext: Context, ctx: Context) -> Context:
+    '''Returns the subset of <ctx> which is references in <subcontext>'''
+    refs = []
+    for exp in subcontext:
+        for token in exp.content:
+            if token.type == 'ref':
+                refs += resolve_reference(token.value, context)
+    return frozenset(refs)
+
+
+def parse_indent(exp: Expression) -> int:
+    '''Returns the amount of space before a "-" in an expression's content'''
     if exp.content[0].type != 'indent':
         return 0
     return exp.content[0].value.index('-')
 
 
 def get_exp_parent(child_exp: Expression, ctx: Context) -> Optional[Expression]:
+    '''Returns the first less-indented expression preceeding <child_exp> in <ctx>'''
     sorted_ctx = sort_by_property(ctx, 'index')
-    child_indent = get_indent(child_exp)
+    child_indent = parse_indent(child_exp)
     preceeding_ctx = sorted_ctx[:sorted_ctx.index(child_exp)]
     for exp in preceeding_ctx[::-1]:
-        if child_indent > get_indent(exp):
+        if child_indent > parse_indent(exp):
             return exp
     return None
 
 
 def get_parent(child_ctx: Context, ctx: Context) -> Context:
+    '''Returns the subset of <ctx> with parents in <child_ctx>'''
     parents = []
     for exp in child_ctx:
         parents.append(get_exp_parent(exp, ctx))
@@ -154,7 +178,7 @@ def get_parent(child_ctx: Context, ctx: Context) -> Context:
 
 
 def get_exp_children(parent_exp: Expression, ctx: Context) -> Context:
-    '''Returns all blocks which have <parent> as their parent.'''
+    '''Returns the subset of <ctx> which has <parent> as their parent'''
     sorted_ctx = sort_by_property(ctx, 'index')
     if parent_exp not in ctx:
         return frozenset([])
@@ -167,17 +191,25 @@ def get_exp_children(parent_exp: Expression, ctx: Context) -> Context:
 
 
 def get_children(parent_ctx: Context, ctx: Context) -> Context:
+    '''Returns the subset of <ctx> with a parent in <parent_ctx>'''
     children = []
     for exp in ctx:
         if get_exp_parent(exp, ctx) in parent_ctx:
             children.append(exp)
     return frozenset(children)
 
-for exp in sort_by_property(context, 'index'):
-    print(generate_id(exp), exp)
 
-print('======')
-a = resolve_id('1455753487410189595', context)
-print(a)
-print('------')
-print(get_children(a, context))
+def get_links_to(subcontext: Context, ctx: Context, link_func: Linker) -> Context:
+    '''Returns the subset of <ctx> which links into <subcontext> according to <link_func>'''
+    linked = []
+    for exp in ctx:
+        for l in link_func(frozenset([exp]), ctx):
+            if l in subcontext:
+                linked.append(exp)
+                break
+    return frozenset(linked)
+
+
+for exp in sort_by_property(context, 'index'):
+    print(parse_id(exp), exp)
+
